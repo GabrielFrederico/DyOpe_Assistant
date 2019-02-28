459e351f90be6a95f0133f4514417e50d414cf58
JuanJGB <juanbenini123@gmail.com>
2019-02-28 16:55:13 -0300

    Continuando projeto tcc 4ª fase

--- src/main/java/com/projeto/rest/GerenteRest.java
+++ src/main/java/com/projeto/rest/GerenteRest.java
@@ -7,60 +7,74 @@ import org.springframework.web.bind.annotation.*;
 import com.projeto.models.Gerente;
 import com.projeto.repository.GerenteRepository;
 
+import javax.swing.text.html.HTML;
+
 @RestController
 @RequestMapping("gerentes")
 
 public class GerenteRest {
-	
-	@Autowired
-	GerenteRepository gerenteRepository;
-	
-	@RequestMapping(method = RequestMethod.POST, value="/cadastrarGerente")
-	public Gerente save(Gerente gerente) {
-		gerenteRepository.save(gerente);
-		return gerente;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value ="../../../cadastroGerente", method = RequestMethod.GET)
-	public String cadastroGerente(Model model){
-		model.addAttribute("gerente", new Gerente());
-		return "cadastro_gerente";
-	}
 
-	@RequestMapping(value ="/loginGerente", method = RequestMethod.GET)
-		public String loginGerente(Model model){
-		model.addAttribute("login_gerente");
-			return "loginGerente";
-	}
+    @Autowired
+    GerenteRepository gerenteRepository;
 
-	@RequestMapping(method = RequestMethod.GET)
-	   public Iterable<Gerente> listAll(){
-		   return gerenteRepository.findAll();
-	   }
-	
-	 @RequestMapping(method = RequestMethod.GET, path = "/{id}")
-	   public Gerente getGerenteById(@PathVariable("id")long id){
-		   Gerente gerente = gerenteRepository.findById(id);
-		   return gerente;
-	   }
-	   
-	   @RequestMapping(method = RequestMethod.GET, path = "/getByNome/{nome}")
-	   public Gerente getGerenteByNome(@PathVariable("nome") String nome){
-		   Gerente gerente = gerenteRepository.findByNome(nome);
-		   return gerente;
-	   }
-	   
-	   @RequestMapping(method = RequestMethod.PUT)
-		public Gerente update(@RequestBody Gerente gerente) {
-			gerenteRepository.save(gerente);
-			return gerente;
-		}
-	   
-	   @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
-		public Gerente deleteGerenteById(@PathVariable("id") long id) {
-		   Gerente gerente = gerenteRepository.findById(id);
-		   gerenteRepository.delete(gerente);
-			return gerente;
-		}
+    @RequestMapping(method = RequestMethod.POST, value = "/cadastrarGerente")
+    public Gerente save(Gerente gerente) {
+        gerenteRepository.save(gerente);
+        return gerente;
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "../../../cadastroGerente", method = RequestMethod.GET)
+    public String cadastroGerente(Model model) {
+        model.addAttribute("gerente", new Gerente());
+        return "cadastro_gerente";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+    @RequestMapping(value = "/loginGerente", method = RequestMethod.GET)
+    public Model loginGerente(Model model) {
+        model.addAttribute("login_gerente");
+        return model;
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<Gerente> listAll() {
+        return gerenteRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public Gerente getGerenteById(@PathVariable("id") long id) {
+        Gerente gerente = gerenteRepository.findById(id);
+        return gerente;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getByNome/{nome}")
+    public Gerente getGerenteByNome(@PathVariable("nome") String nome) {
+        Gerente gerente = gerenteRepository.findByNome(nome);
+        return gerente;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public Gerente update(@RequestBody Gerente gerente) {
+        gerenteRepository.save(gerente);
+        return gerente;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    public Gerente deleteGerenteById(@PathVariable("id") long id) {
+        Gerente gerente = gerenteRepository.findById(id);
+        gerenteRepository.delete(gerente);
+        return gerente;
+    }
 }
