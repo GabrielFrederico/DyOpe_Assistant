365d009fffb5aa0c75ef9f150aad6f359c9b6468
JuanJGB <juanbenini123@gmail.com>
2019-02-28 15:08:33 -0300

    Continuando projeto tcc 4ª fase

--- src/main/java/com/projeto/rest/OperacaoRest.java
+++ src/main/java/com/projeto/rest/OperacaoRest.java
@@ -1,7 +1,9 @@
 package com.projeto.rest;
 
+import com.projeto.models.Operacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.repository.OperacaoRepository;
@@ -12,4 +14,15 @@ public class OperacaoRest {
 
 	@Autowired
 	OperacaoRepository operacaoRepository;
+
+	@RequestMapping(method = RequestMethod.POST, value="/cadastrarOperacao")
+	public Operacao save(Operacao operacao) {
+		operacaoRepository.save(operacao);
+		return operacao;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public  Iterable<Operacao> listAll(){
+		return operacaoRepository.findAll();
+	}
 }
