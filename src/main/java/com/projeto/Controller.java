6e3ea163a74b9b0368c4a59d0d59e54459332a7e
JuanJGB <juanbenini123@gmail.com>
2019-03-06 17:05:55 -0300

    Continuando projeto tcc 4ª fase

--- src/main/java/com/projeto/Controller.java
+++ src/main/java/com/projeto/Controller.java
@@ -36,6 +36,12 @@ public class Controller {
 		return "gerente_index";
 	}
 
+	@RequestMapping("/gerentePerfil")
+	public String gerentePerfil() {
+
+		return "perfil";
+	}
+
 	@RequestMapping("/operacaoRisco")
 	public String operacaoRisco() {
 
