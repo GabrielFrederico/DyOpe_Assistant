7289c3bdcf5c711931d6da89413ad6994acc5b23
JuanJGB <juanbenini123@gmail.com>
2019-04-03 16:20:01 -0300

    tcc 4 fase angular

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -5,6 +5,8 @@ import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AppRoutingModule, routingComponent, routingComponent2} from './app-routing.module';
 import { OperacaoRiscoComponent } from './operacao-risco/operacao-risco.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
 
 @NgModule({
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -12,6 +14,8 @@ import { OperacaoRiscoComponent } from './operacao-risco/operacao-risco.componen
         AppComponent,
        routingComponent,
       routingComponent2,
+        SidebarComponent,
+        NavBarComponent
     ],
     imports: [
         AppRoutingModule,
