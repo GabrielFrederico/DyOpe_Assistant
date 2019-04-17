0eeec8e2074d6c4d69d7bd38ab7cea45c66a7b37
JuanJGB <juanbenini123@gmail.com>
2019-04-17 14:56:23 -0300

    SAMALAMADUMA

---
+++ src/main/angular/src/app/service/infosetor.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export class Infosetor {
+  public setor: string;
+  public operacao: string;
+  public qtdPecasOpe: string;
+  public tempoNecessario: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfosetorService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getInfosetor() {
+    return this.httpClient.get<Infosetor[]>('http://localhost:8080/cadastrarinfosetor');
+  }
+
+  cadastrarInfosetor(infosetor: Infosetor): Observable<Infosetor> {
+    return this.httpClient.post<Infosetor>('http://localhost:8080/cadastrar/cadastrarinfosetor', infosetor);
+  }
+}
+
