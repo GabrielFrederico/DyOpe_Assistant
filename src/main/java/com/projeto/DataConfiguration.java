482adc846619a60031e2cb4ecf60cb1716e40a5b
JuanJGB <juanbenini123@gmail.com>
2019-05-08 14:21:34 -0300

    projeto tcc spring boot angular

--- src/main/java/com/projeto/DataConfiguration.java
+++ src/main/java/com/projeto/DataConfiguration.java
@@ -16,9 +16,9 @@ public class DataConfiguration {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/dbdyopeassistant2");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/dbdyopeassistant");
 		dataSource.setUsername("root");
-		dataSource.setPassword("root");
+		dataSource.setPassword("");
 		return dataSource;
 	}
 
